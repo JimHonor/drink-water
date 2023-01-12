2f8fff08889c6191f5cf62b86560f099085156df
Ye <ye66169@gmail.com>
2023-01-12 18:26:36 +0800

    【重构】Make the spacing between the 250ml bottles the same.

--- src/components/Bottles.jsx
+++ src/components/Bottles.jsx
@@ -1,4 +1,4 @@
-import { Row, Col, Typography } from "antd";
+import { Row, Col, Typography, Space } from "antd";
 const { Text } = Typography;
 
 export default function Bottles({ count, onDrinkClick }) {
@@ -14,33 +14,33 @@ export default function Bottles({ count, onDrinkClick }) {
           Select how many water glasses you had
         </Text>
       </div>
-      <Row
-        gutter={[16, 16]}
-        style={{ width: "320px", marginLeft: "auto", marginRight: "auto" }}
-      >
-        {new Array(8).fill(null).map((e, index) => (
-          <Col key={index} span={6}>
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-                width: "40px",
-                height: "80px",
-                margin: "0 auto",
-                border: "2px solid #144fc6",
-                borderRadius: "0 0 12px 12px",
-                ...styleBottle(index < count),
-              }}
-              onClick={() => onDrinkClick(index + 1)}
-            >
-              <span>250</span>
-              <span>ml</span>
-            </div>
-          </Col>
+      <Space direction="vertical" size="middle">
+        {[null, null].map((_, index) => (
+          <Space key={index} size="middle">
+            {new Array(4).fill(null).map((_, index) => (
+              <div
+                key={index}
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  width: "40px",
+                  height: "80px",
+                  margin: "0 auto",
+                  border: "2px solid #144fc6",
+                  borderRadius: "0 0 12px 12px",
+                  ...styleBottle(index < count),
+                }}
+                onClick={() => onDrinkClick(index + 1)}
+              >
+                <span>250</span>
+                <span>ml</span>
+              </div>
+            ))}
+          </Space>
         ))}
-      </Row>
+      </Space>
     </>
   );
 }
